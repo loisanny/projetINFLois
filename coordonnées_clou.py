f22cf1afde0ddbfc65f0ef31bc5f058f6fa7b667
loisanny <162833213+loisanny@users.noreply.github.com>
2024-04-11 17:07:57 -0400

    Update coordonnées_clou.py

--- coordonnées_clou.py
+++ coordonnées_clou.py
@@ -1,5 +1,22 @@
 def calculer_coordonnees_clou(A, B, C, D, E):
 
+    """
+      Calcule les coordonnées des points d'un assemblage de clouage.
+
+      Arguments :
+          A (float): Description de l'élément A.
+          B (float): Description de l'élément B.
+          C (float): Description de l'élément C.
+          D (float): Description de l'élément D.
+          E (float): Description de l'élément E.
+
+      Retourne :
+          list: Liste des coordonnées des points.
+
+      Exemple d'utilisation :
+          points = calculer_coordonnees_clou(10, 5, 3, 2, 4)
+      """
+
     # Calcul des coordonnées des points
     pt_0 = (-B/2, C/2)
     pt_1 = (-B/2, -C/2)
@@ -30,6 +47,33 @@ from transformation_geometrique import calculer_inclinaison_point
 
 def appliquer_transformation_clou(points_clou, center_rotation, angle_rotation,
                                  direction_inclinaison, angle_inclinaison, axe_reflexion):
+                                     
+     """
+       Applique différentes transformations géométriques à un clou.
+
+       Cette fonction prend en entrée une liste de points qui nous indiquent la  position du clou dans l'espace ainsi
+       que les paramètres pour effectuer de transformations géométriques telles que la réflexion, la rotation et
+       l'inclinaison. Elle applique ensuite ce transformations aux points et retourne les coordonnées des
+       points après chaque transformation.
+
+       Arguments :
+           points_clou (list): Liste des points de clouage, chaque élément étant un tuple contenant le nom du point et
+           ses coordonnées sous la forme (x, y).
+           center_rotation (tuple): Coordonnées du centre de rotation sous forme de tuple (x, y).
+           angle_rotation (float): Angle de rotation en degrés.
+           direction_inclinaison (str): Direction de l'inclinaison, 'horizontal' ou 'vertical'.
+           angle_inclinaison (float): Angle d'inclinaison en degrés.
+           axe_reflexion (str): Axe de réflexion, 'x' ou 'y'.
+
+       Retourne :
+           tuple: Un tuple contenant trois listes de tuples, chacune représentant les coordonnées des points du clou
+                  après chaque transformation. Les listes correspondent respectivement à la réflexion, la rotation et
+                  l'inclinaison.
+
+       Exemple d'utilisation :
+           reflexion, rotation, inclinaison = appliquer_transformation_clou(points_clou, (0, 0), 90, 'vertical', 45, 'y')
+       """
+                                     
     # initialisation de listes vides qui contiendront les tuples de coordonées des points
     # après les transformations géométriques
     coord_point_reflexion = []
